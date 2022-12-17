19624c4c835741b027ad01d2c823d83b28ddaa20
Roy Buitenhuis <roy.buitenhuis94@gmail.com>
2022-12-17 16:06:33 +0100

    Add types

--- MapIcons.lua
+++ MapIcons.lua
@@ -1,70 +1,72 @@
+local AZP_PIN_TYPE_GLYPH = 1
+local AZP_PIN_TYPE_TRANSMORGAPHY = 1
 local pins =
 {
     [2022] = -- The Waking Shores
     {
-        {AchieID = 15985, PosX = 75.25, PosY = 56.99, Name = "Skytop Observatory"},
-        {AchieID = 15986, PosX = 74.90, PosY = 37.64, Name = "Wingrest Embassy"},
-        {AchieID = 15987, PosX = 40.95, PosY = 71.86, Name = "Obsidian Bulwark"},
-        {AchieID = 15988, PosX = 54.46, PosY = 74.21, Name = "Ruby Life Pools"},
-        {AchieID = 15989, PosX = 46.40, PosY = 52.14, Name = "The Overflowing Spring"},
-        {AchieID = 15990, PosX = 52.64, PosY = 17.14, Name = "Life-Binder Observatory"},
-        {AchieID = 15991, PosX = 57.66, PosY = 54.93, Name = "Crumbling Life Archway"},
-        {AchieID = 16051, PosX = 69.32, PosY = 46.20, Name = "Dragonheart Outpost"},
-        {AchieID = 16052, PosX = 73.14, PosY = 20.66, Name = "Scalecracker Peak"},
-        {AchieID = 16053, PosX = 21.88, PosY = 51.50, Name = "Obsidian Throne"},
-        {AchieID = 16668, PosX = 74.33, PosY = 57.65, Name = "Skytop Observatory Rostrum"},
-        {AchieID = 16669, PosX = 58.12, PosY = 78.61, Name = "Flashfrost Enclave"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 15985, PosX = 75.25, PosY = 56.99, Name = "Skytop Observatory"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 15986, PosX = 74.90, PosY = 37.64, Name = "Wingrest Embassy"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 15987, PosX = 40.95, PosY = 71.86, Name = "Obsidian Bulwark"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 15988, PosX = 54.46, PosY = 74.21, Name = "Ruby Life Pools"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 15989, PosX = 46.40, PosY = 52.14, Name = "The Overflowing Spring"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 15990, PosX = 52.64, PosY = 17.14, Name = "Life-Binder Observatory"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 15991, PosX = 57.66, PosY = 54.93, Name = "Crumbling Life Archway"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16051, PosX = 69.32, PosY = 46.20, Name = "Dragonheart Outpost"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16052, PosX = 73.14, PosY = 20.66, Name = "Scalecracker Peak"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16053, PosX = 21.88, PosY = 51.50, Name = "Obsidian Throne"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16668, PosX = 74.33, PosY = 57.65, Name = "Skytop Observatory Rostrum"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16669, PosX = 58.12, PosY = 78.61, Name = "Flashfrost Enclave"},
 
-        {AchieID = 16670, PosX = 48.80, PosY = 86.64, Name = "Rubyscale Outpost"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16670, PosX = 48.80, PosY = 86.64, Name = "Rubyscale Outpost"},
     },
     [2023] = -- Ohn'Ahran Plains
     {
-        {AchieID = 16054, PosX = 57.93, PosY = 31.24, Name = "Ohn'Ahra's Roost"},
-        {AchieID = 16055, PosX = 30.51, PosY = 36.00, Name = "Nokhudon Hold"},
-        {AchieID = 16056, PosX = 30.15, PosY = 61.40, Name = "Emerald Gardens"},
-        {AchieID = 16057, PosX = 29.55, PosY = 75.24, Name = "The Eternal Kurgans"},
-        {AchieID = 16058, PosX = 44.69, PosY = 64.61, Name = "Szar Skeleth"},
-        {AchieID = 16059, PosX = 47.23, PosY = 72.36, Name = "Mirror of the Sky"},
-        {AchieID = 16060, PosX = 57.08, PosY = 80.12, Name = "Ohn'Iri Springs"},
-        {AchieID = 16061, PosX = 84.43, PosY = 77.60, Name = "Dragonsprings Summit"},
-        {AchieID = 16062, PosX = 86.49, PosY = 39.42, Name = "Rusza'Thar Reach"},
-        {AchieID = 16063, PosX = 61.51, PosY = 64.22, Name = "Windsong Rise"},
-        {AchieID = 16670, PosX = 79.96, PosY = 13.16, Name = "Rubyscale Outpost"},
-        {AchieID = 16671, PosX = 78.36, PosY = 21.28, Name = "Mirewood Fen"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16054, PosX = 57.93, PosY = 31.24, Name = "Ohn'Ahra's Roost"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16055, PosX = 30.51, PosY = 36.00, Name = "Nokhudon Hold"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16056, PosX = 30.15, PosY = 61.40, Name = "Emerald Gardens"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16057, PosX = 29.55, PosY = 75.24, Name = "The Eternal Kurgans"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16058, PosX = 44.69, PosY = 64.61, Name = "Szar Skeleth"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16059, PosX = 47.23, PosY = 72.36, Name = "Mirror of the Sky"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16060, PosX = 57.08, PosY = 80.12, Name = "Ohn'Iri Springs"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16061, PosX = 84.43, PosY = 77.60, Name = "Dragonsprings Summit"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16062, PosX = 86.49, PosY = 39.42, Name = "Rusza'Thar Reach"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16063, PosX = 61.51, PosY = 64.22, Name = "Windsong Rise"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16670, PosX = 79.96, PosY = 13.16, Name = "Rubyscale Outpost"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16671, PosX = 78.36, PosY = 21.28, Name = "Mirewood Fen"},
     },
     [2024] = -- Azure Span
     {
-        {AchieID = 16064, PosX = 45.84, PosY = 25.66, Name = "Cobalt Assembly"},
-        {AchieID = 16065, PosX = 39.21, PosY = 62.91, Name = "Azure Archive"},
-        {AchieID = 16066, PosX = 68.64, PosY = 60.27, Name = "Ruins of Karnthar"},
-        {AchieID = 16067, PosX = 70.56, PosY = 46.25, Name = "Lost Ruins"},
-        {AchieID = 16068, PosX = 10.35, PosY = 35.91, Name = "Brackenhide Hollow"},
-        {AchieID = 16069, PosX = 26.74, PosY = 31.68, Name = "Creektooth Den"},
-        {AchieID = 16070, PosX = 60.59, PosY = 69.96, Name = "Imbu"},
-        {AchieID = 16071, PosX = 53.02, PosY = 49.13, Name = "Zelthrak Outpost"},
-        {AchieID = 16072, PosX = 67.64, PosY = 29.12, Name = "Kalthraz Fortress"},
-        {AchieID = 16073, PosX = 72.45, PosY = 39.73, Name = "Vakthros Range"},
-        {AchieID = 16672, PosX = 70.13, PosY = 86.80, Name = "Forkriver Crossing"},
-        {AchieID = 16673, PosX = 56.81, PosY = 16.06, Name = "The Fallen Course"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16064, PosX = 45.84, PosY = 25.66, Name = "Cobalt Assembly"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16065, PosX = 39.21, PosY = 62.91, Name = "Azure Archive"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16066, PosX = 68.64, PosY = 60.27, Name = "Ruins of Karnthar"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16067, PosX = 70.56, PosY = 46.25, Name = "Lost Ruins"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16068, PosX = 10.35, PosY = 35.91, Name = "Brackenhide Hollow"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16069, PosX = 26.74, PosY = 31.68, Name = "Creektooth Den"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16070, PosX = 60.59, PosY = 69.96, Name = "Imbu"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16071, PosX = 53.02, PosY = 49.13, Name = "Zelthrak Outpost"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16072, PosX = 67.64, PosY = 29.12, Name = "Kalthraz Fortress"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16073, PosX = 72.45, PosY = 39.73, Name = "Vakthros Range"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16672, PosX = 70.13, PosY = 86.80, Name = "Forkriver Crossing"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16673, PosX = 56.81, PosY = 16.06, Name = "The Fallen Course"},
     },
     [2025] = -- Thaldraszus
     {
-        {AchieID = 16098, PosX = 66.00, PosY = 82.34, Name = "Temporal Conflux"},
-        {AchieID = 16099, PosX = 46.00, PosY = 74.07, Name = "Stormshroud Peak"},
-        {AchieID = 16100, PosX = 35.56, PosY = 85.54, Name = "South Hold Gate"},
-        {AchieID = 16101, PosX = 41.21, PosY = 58.10, Name = "Valdrakken"},
-        {AchieID = 16102, PosX = 49.91, PosY = 40.35, Name = "Algeth'Era"},
-        {AchieID = 16103, PosX = 61.55, PosY = 56.60, Name = "Tyrhold"},
-        {AchieID = 16104, PosX = 62.41, PosY = 40.46, Name = "Algeth'Ar Academy"},
-        {AchieID = 16105, PosX = 67.11, PosY = 11.77, Name = "Veiled Ossuary"},
-        {AchieID = 16106, PosX = 72.39, PosY = 51.45, Name = "Vault of the Incarnates"},
-        {AchieID = 16107, PosX = 72.93, PosY = 69.21, Name = "Thaldrazsus Apex"},
-        {AchieID = 16666, PosX = 52.69, PosY = 67.42, Name = "Gelikyr Overlook"},
-        {AchieID = 16667, PosX = 55.69, PosY = 72.21, Name = "Passage of Time"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16098, PosX = 66.00, PosY = 82.34, Name = "Temporal Conflux"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16099, PosX = 46.00, PosY = 74.07, Name = "Stormshroud Peak"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16100, PosX = 35.56, PosY = 85.54, Name = "South Hold Gate"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16101, PosX = 41.21, PosY = 58.10, Name = "Valdrakken"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16102, PosX = 49.91, PosY = 40.35, Name = "Algeth'Era"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16103, PosX = 61.55, PosY = 56.60, Name = "Tyrhold"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16104, PosX = 62.41, PosY = 40.46, Name = "Algeth'Ar Academy"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16105, PosX = 67.11, PosY = 11.77, Name = "Veiled Ossuary"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16106, PosX = 72.39, PosY = 51.45, Name = "Vault of the Incarnates"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16107, PosX = 72.93, PosY = 69.21, Name = "Thaldrazsus Apex"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16666, PosX = 52.69, PosY = 67.42, Name = "Gelikyr Overlook"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16667, PosX = 55.69, PosY = 72.21, Name = "Passage of Time"},
     },
     [2112] = -- Valdrakken
     {
-        {AchieID = 16101, PosX = 59.31, PosY = 37.28, Name = "Valdrakken"},
+        {Type = AZP_PIN_TYPE_GLYPH, AchieID = 16101, PosX = 59.31, PosY = 37.28, Name = "Valdrakken"},
     }
 }
 
@@ -94,11 +96,18 @@ function DragonMapDataProviderMixin:RefreshAllData()
     self:RemoveAllData()
     if pinsForMap then
         for i, pinInfo in ipairs(pinsForMap) do
-            local id, name, points, completed = GetAchievementInfo(pinInfo.AchieID)
-            if completed  == false and not AZPHideGlyphs == true then
+            if pinInfo.Type == AZP_PIN_TYPE_GLYPH then
+                
+                local id, name, points, completed = GetAchievementInfo(pinInfo.AchieID)
+                if completed  == false and not AZPHideGlyphs == true then
+                    local pin = self:GetMap():AcquirePin(self:GetPinTemplate())
+                    pin:SetData(pinInfo)
+                end
+            else
                 local pin = self:GetMap():AcquirePin(self:GetPinTemplate())
-                pin:SetPosition(pinInfo.PosX / 100, pinInfo.PosY / 100)
+                pin:SetData(pinInfo)
             end
+
         end
     else
         self:RemoveAllData();
@@ -115,16 +124,23 @@ end
 
 DragonMapPinMixin = CreateFromMixins(MapCanvasPinMixin)
 function DragonMapPinMixin:OnLoad()
-    self.Texture:SetTexture("Interface/ICONS/Ability_DragonRiding_Glyph01")
+    self.HighlightTexture:SetSize(100, 100)
+    self:UseFrameLevelType("PIN_FRAME_LEVEL_TOPMOST")
+    self:SetScaleStyle()
+end
+
+function  DragonMapPinMixin:SetData(pinInfo)
+    self:SetPosition(pinInfo.PosX / 100, pinInfo.PosY / 100)
+    if pinInfo.Type == AZP_PIN_TYPE_GLYPH then
+        self.Texture:SetTexture("Interface/ICONS/Ability_DragonRiding_Glyph01")
+    elseif pinInfo.Type == AZP_PIN_TYPE_TRANSMORGAPHY then
+    end
     self.Texture:SetSize(100, 100)
     self.Mask = self:CreateMaskTexture()
     self.Mask:SetPoint("CENTER", self.Texture, "CENTER", 0, 0)
     self.Mask:SetTexture("Interface/CHARACTERFRAME/TempPortraitAlphaMask", "CLAMPTOBLACKADDITIVE", "CLAMPTOBLACKADDITIVE")
     self.Mask:SetSize(90, 90)
     self.Texture:AddMaskTexture(self.Mask)
-    self.HighlightTexture:SetSize(100, 100)
-    self:UseFrameLevelType("PIN_FRAME_LEVEL_TOPMOST")
-    self:SetScaleStyle()
 end
 
 function DragonMapPinMixin:OnReleased()
